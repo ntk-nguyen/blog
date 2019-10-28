34479c9a97487a99d2137ea83a766f9205a7ee49
Nguyen Nguyen <ntk.nguyen@outlook.com>
2019-10-28 13:38:47 -0500

    add Reducers and Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
+import _ from 'lodash';
 import jsonPlaceHolder from '../api/jsonPlaceHolder';
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceHolder.get('/posts');
-    dispatch({ type: 'FETCH_POSTS', payload: response });
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    _.chain(getState().posts).map('userId').uniq().forEach(id => dispatch(fetchUser(id))).value();
 };
